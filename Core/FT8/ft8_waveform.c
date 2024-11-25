cf039cc064b0118e2cf19a4ea9428e89a29f4481
pwhMass <a203865235@gmail.com>
2024-11-25 21:57:39 +0800

    TMP

--- Core/FT8/ft8_waveform.c
+++ Core/FT8/ft8_waveform.c
@@ -6,3 +6,21 @@
  */
 
 
+#include "ft8_waveform.h"
+
+static uint8_t itones[79] = {
+	    3, 1, 4, 0, 6, 5, 2, 6, 3, 5, 0, 1, 0, 0, 2, 0, 1, 0, 0, 3, 5, 0, 4, 0, 1,
+	    4, 0, 5, 0, 1, 1, 7, 7, 3, 7, 2, 3, 1, 4, 0, 6, 5, 2, 1, 5, 6, 5, 7, 2, 6,
+	    1, 4, 1, 7, 0, 6, 3, 3, 6, 0, 6, 6, 7, 0, 5, 2, 4, 7, 6, 4, 7, 3, 3, 1, 4,
+	    0, 6, 5, 2
+	};
+
+static float gauss_window[ 3*FT8_SYMBOL_TIME_s*FT8_FS_Hz ];
+
+void gauss_window_init(){
+
+}
+
+void ft8_block_compute(symIdx,*buffer){
+
+}
--- Core/FT8/ft8_waveform.h
+++ Core/FT8/ft8_waveform.h
@@ -8,6 +8,13 @@
 #ifndef FT8_FT8_WAVEFORM_H_
 #define FT8_FT8_WAVEFORM_H_
 
+#define FT8_SYMBOL_TIME_s 0.16
+#define FT8_SYMBOL_BIN_Hz 6.25
+#define FT8_FS_Hz 2000
+#define FT8_F0_Hz 100
+#define FT8_GAUSS_WINDOW_BT 2
+
+#define FT_SYMBOL_NUMBER 79
 
 
 #endif /* FT8_FT8_WAVEFORM_H_ */
